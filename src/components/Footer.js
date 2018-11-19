5e878112bd955efeff3ba4b9509369da8d90cefc
Ola Vea <ola@olavea.com>
2018-11-19 09:27:49 +0100

    Logo of "studio5dekor" and links to facebag and website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,28 @@
 import React from "react";
+//import Link from "link";
 
 const Footer = ({ children }) => (
-  <footer className="footer has-text-centered">{children}</footer>
+  <footer className="footer has-text-centered">
+    {children}
+    <p>
+      <a href="https://www.facebook.com/Studio-5-Dekor-as-290884624375196/">
+        Studio 5 Dekor as på facebook
+      </a>
+    </p>
+
+    <p>
+      <a href="https://studio5dekor.no">www.studio5dekor.no,</a>
+    </p>
+  </footer>
 );
+//<p>'Vi gjør andre ting også, se:'</p>
+//Hvordan får jeg mellomrom?
+//const linkToWebsite = () => {
+// "https://studio5dekor.no/" https://studio5dekor.no/
+//};
+
+//const linkToFacebook = () => {
+// "studio5dekor på facebook" https://fb.com/studio5dekor.no/
+//};
 
 export default Footer;
