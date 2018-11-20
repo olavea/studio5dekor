ffd0ff06a5306ae5f93dc7aeeaf9158649390b70
Ola Vea <ola@olavea.com>
2018-11-20 13:42:42 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,6 @@ import React from "react";
 const Footer = ({ children }) => (
   <footer className="footer has-text-centered">
     {children}
-//    <p>
-//      <a href="https://www.facebook.com/Studio-5-Dekor-as-290884624375196/">
-//        Studio 5 Dekor as på facebook
-//      </a>
-//    </p>
 
     <p>
       <a href="https://lillylabs.no">www.lillylabs.no,</a>
@@ -24,5 +19,12 @@ const Footer = ({ children }) => (
 //const linkToFacebook = () => {
 // "studio5dekor på facebook" https://fb.com/studio5dekor.no/
 //};
+// was at line 7
+//    <p>
+//      <a href="https://www.facebook.com/Studio-5-Dekor-as-290884624375196/">
+//        Studio 5 Dekor as på facebook
+//      </a>
+//    </p>
+
 
 export default Footer;
